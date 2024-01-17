abc26c75df2ccbc17ce27371fbe72fb0476b1eb2
Michael Lau <lauchunshekmichael@gmail.com>
2024-01-17 09:46:26 -0700

    class changes

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
-    }
+
+        //list - new keyword
+        List<int> myInts = new List<int>();
+        var moreInt = new List<int>();
+
+        moreInt.Add(34);
+        moreInt.Add(10);
+
+        //Iterate over items
+        foreach(var n in moreInt) {
+            System.Console.WriteLine($"n = {n}");
+        }
+     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+    //Functions
+
+    //     retuenType FunctionName (paramType paramName1, paramType paramName2)
+    //     {
+    //         FunctionName body
+    //     }
+
+        int Add2(int number) {
+            return number + 2;
+        }
+
+        int more = Add2(10);
+
+    //void
+        void PrintName(string name) {
+            if (name.Length == 0) {
+                return;
+            }
+            System.Console.WriteLine($"Name is {name}");
+            return;
+        }
+
+        PrintName("Bob");
+        
+
+    
+    
     }
+
 }
