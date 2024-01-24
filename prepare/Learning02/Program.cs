70601ce481828058a12762accbfff9ff3b430913
Michael Lau <lauchunshekmichael@gmail.com>
2024-01-24 09:02:00 -0700

    changes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,12 +4,27 @@ public class Car {
     public int gallons;
     public string make;
     public string model;
+
+    public Owner owner;
+
+    public Car(string make, string model, int gallons, int milesPerGallon, Owner owner){
+        this.make = make;
+        this.model = model;
+        this.gallons = gallons;
+        this.milesPerGallon = milesPerGallon;
+        this.owner = owner;
+    }
     public int TotalRange() {
         return gallons * milesPerGallon;
     }
     public void Display() {
-        System.Console.WriteLine($"{make} {model}: Range= {TotalRange()}");
+        System.Console.WriteLine($"{make} {model} {owner.name}: Range= {TotalRange()}");
     }
+}
+
+public class Owner{
+    public string name;
+    public string phone;
 }
 
 class Program
@@ -18,11 +33,15 @@ class Program
     {   
         var cars = new List<Car>();
 
-        var car = new Car();
+        var car = new Car("Honda", "Civic", 10,30,owner);
             car.make = "Honda";
             car.model = "Civic";
             car.gallons = 10;
             car.milesPerGallon = 30;
+            var owner = new Owner();
+            owner.name = "Michael";
+            owner.phone = "302-4030";
+            car.owner = owner;
             cars.Add(car);
 
             car = new Car();
@@ -30,13 +49,16 @@ class Program
             car.model = "F-150";
             car.gallons = 30;
             car.milesPerGallon = 5;
+            owner = new Owner();
+            owner.name = "Brendan";
+            owner.phone = "405-2048";
             cars.Add(car);
 
             foreach (var c in cars)
             {
                 // System.Console.WriteLine($"{c.make} {c.model}: Range = {c.gallons * c.milesPerGallon}");
                 c.Display();
-                int range = c.TotalRange
+                int range = c.TotalRange();
             }
     }
 }
