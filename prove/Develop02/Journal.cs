51286b67a51ac47b0d83a0f769ee24aeecad73f1
Michael Lau <lauchunshekmichael@gmail.com>
2024-01-29 13:40:47 -0700

    hw

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,20 @@
+public class journal {
+    public List<Entry> entries;
+    
+
+    public journal() {
+
+    }
+
+    public journal(string import) {
+            
+    }
+
+    public void Display() {
+
+    }
+
+    public string Export() {
+        
+    }
+}
