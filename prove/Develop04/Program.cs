f97fa8104feb4d27f50fb26b49140a5c05199d25
Brolelel <lauchunshekmichael@gmail.com>
2024-03-02 17:44:26 -0700

    hw changes

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -10,10 +10,10 @@ class Program
         Listing listing= new Listing();
         do{
             Console.WriteLine("Menu Options:");
-            Console.WriteLine("    1.Start breathing activity");
-            Console.WriteLine("    2.Start reflecting activity");
-            Console.WriteLine("    3.Start listing activity");
-            Console.WriteLine("    4.Quit");
+            Console.WriteLine("-> 1.Start breathing activity");
+            Console.WriteLine("-> 2.Start reflecting activity");
+            Console.WriteLine("-> 3.Start listing activity");
+            Console.WriteLine("-> 4.Quit");
             entry = Console.ReadLine();
 
             if(entry =="1" ){
